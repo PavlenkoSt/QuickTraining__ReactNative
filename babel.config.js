9a65bafa6864e29ecf34f1183f60cf3a92959115
Stanislav <verbatim8872@gmail.com>
2022-04-17 18:49:22 +0300

    configure env

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-};
+  presets: ['module:metro-react-native-babel-preset', 'mobx'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.android.js',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.tsx',
+        ],
+        root: ['.'],
+      },
+    ],
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    'react-native-reanimated/plugin',
+  ],
+}
---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react'
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View>
+          <Text>Init</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default App
