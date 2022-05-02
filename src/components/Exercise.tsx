94945a6f5cf1cc30e9420164719883efd41a14dd
Stanislav <verbatim8872@gmail.com>
2022-05-02 15:39:12 +0300

    connect video in exercise

--- src/components/Exercise.tsx
+++ src/components/Exercise.tsx
@@ -1,6 +1,7 @@
-import { View } from 'react-native'
+import { Dimensions, View } from 'react-native'
 import React from 'react'
 import { EStyleSheet } from 'react-native-extended-stylesheet-typescript'
+import VideoPlayer from 'react-native-video-player'
 
 import CustomText from './CustomText'
 
@@ -8,10 +9,39 @@ const Exercise = () => {
   return (
     <View>
       <CustomText>Ex</CustomText>
+      <VideoPlayer
+        video={require('src/assets/videos/ex/push-up.mp4')}
+        videoWidth={1600}
+        videoHeight={900}
+        loop={true}
+        repeat={true}
+        autoplay={true}
+        muted={true}
+        onError={(e) => console.log(e)}
+        disableFullscreen={true}
+        disableControlsAutoHide={true}
+        fullScreenOnLongPress={false}
+        customStyles={{
+          controls: {
+            display: 'none',
+          },
+          seekBar: {
+            display: 'none',
+          },
+        }}
+      />
     </View>
   )
 }
 
 export default Exercise
 
-const styles = EStyleSheet.create({})
+const { width } = Dimensions.get('window')
+
+const styles = EStyleSheet.create({
+  video: {
+    width,
+    height: 200,
+    flex: 1,
+  },
+})
